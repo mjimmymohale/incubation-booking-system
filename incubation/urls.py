352c7ad894bcc43573f0871ec77da91469badeb2
mjimmymohale <mjimmymohale@gmail.com>
2025-02-25 09:06:28 +0200

    Update urls.py

--- incubation/urls.py
+++ incubation/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+# Simple home page view
+def home(request):
+    return HttpResponse("<h1>Welcome to the Incubation Booking System</h1>")
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
 ]

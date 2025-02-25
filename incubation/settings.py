cd2a5d50e5369645293b53dfbeafaeeee43b9a41
mjimmymohale <mjimmymohale@gmail.com>
2025-02-25 08:49:09 +0200

    Update settings.py

--- incubation/settings.py
+++ incubation/settings.py
@@ -22,6 +22,21 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 ROOT_URLCONF = 'incubation.urls'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    }
+]
 WSGI_APPLICATION = 'incubation.wsgi.application'
 DATABASES = {
     'default': {

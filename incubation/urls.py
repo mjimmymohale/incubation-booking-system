0b90b3ae75de54b5791cebcd29d76e24513c3182
mjimmymohale <mjimmymohale@gmail.com>
2025-02-25 09:21:37 +0200

    Update urls.py

--- incubation/urls.py
+++ incubation/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from django.http import HttpResponse
 # Simple home page view
 def home(request):
-    return HttpResponse("<h1>Welcome to the Incubation Booking System</h1>")
+    return HttpResponse("<h1>Welcome to the University of Johannesburg Technology Transfer officer (UJTTO) Entrepreneurship Support and Facilities Incubation Booking System</h1>")
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
+    path('auth/', include('authapp.urls')),
 ]
